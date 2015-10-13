899a9f2aa93241bac359be4598bc9e751c626ed9
slin63 <slin63@illinois.edu>
2015-10-12 22:51:16 -0500

    what am i doing still tbh

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #Scrapy-test
+
+To start a shell, you must go to the project’s top level directory and run:
+
+e.g.
+    $ scrapy shell "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/"
+
--- tutorial/spiders/spider.py
+++ tutorial/spiders/spider.py
@@ -1,15 +1,24 @@
 import scrapy
 
+from tutorial.items import DmozItem # From directory.[file.py] import class
 
 class DmozSpider(scrapy.Spider):
-    name = 'dmoz' # Name of the spider . . . must be unique
+    name = "dmoz" # Name of the spider . . . must be unique
     allowed_domains = ["dmoz.org"]
     start_urls = [ # Where the spider will begin crawling / first pages downloaded
-         "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
     ]
 
-    def parse(self, response): #called with the downloaded Response object of each start URL
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+    def parse(self, response): # Handles the main links on the directory page
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response): # called with the downloaded Response object of each start URL
+        for sel in response.xpath('//ul/li'):  # For each selection element in the path //ul/li
+            item = DmozItem() # From items.py, custom dict type used to store scrapy elements
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            yield item
+            # print '{ TITLE = %s }\n{ URL = %s }\n{ DESC = %s }\n\n'%(title, url, desc)
